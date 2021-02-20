26cb138b32af85c5fc72272518888b1272d61620
inmanturbo <inman.turbo@gmail.com>
2021-02-20 14:54:04 -0500

    wip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,26 +2,26 @@
 
 set -e
 
-GIT_USER=${USER:-fake_user}
+GIT_USER=${GIT_USER:-fake_user}
 
-GIT_SSH_KEY=${SSH_KEY:-fake_key}
+GIT_SSH_KEY=${GIT_SSH_KEY:-fake_key}
 
-GIT_SSH_CONFIG=${SSH_CONFIG:-fake_config}
+GIT_SSH_CONFIG=${GIT_SSH_CONFIG:-fake_config}
 
-GIT_EMAIL=${EMAIL:-fake@fake.com}
+GIT_EMAIL=${GIT_EMAIL:-fake@fake.com}
 
 
 # Copy SSH key for git private repos
 mkdir -p /root/.ssh
-echo "${SSH_KEY}" > /root/.ssh/id_rsa
+echo "${GIT_SSH_KEY}" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
 
-echo "${SSH_CONFIG}" > /root/.ssh/config
+echo "${GIT_SSH_CONFIG}" > /root/.ssh/config
 chmod 600 /root/.ssh/config
 
-git config --global user.email "${EMAIL}"
-git config --global user.name "${USER}"
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_USER}"
 
 CMD="/workdir/db-dumper.sh ${DUMPER_COMMAND}"
   
